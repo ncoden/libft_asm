8f6a0ba9031a0c94b4e9416e5014a0778549c273
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-02 16:49:39 +0200

    Add (null) print for ft_puts on NULL pointer

--- src/ft_puts.s
+++ src/ft_puts.s
@@ -6,12 +6,9 @@
 ;    By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2015/04/29 17:27:27 by ncoden            #+#    #+#              ;
-;    Updated: 2015/05/01 17:29:46 by ncoden           ###   ########.fr        ;
+;    Updated: 2015/05/02 15:18:12 by ncoden           ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
-
-%define			CALL_WRITE			(0x2000004)
-%define			STDOUT				(1)
 
 ;	ft_puts
 ;	%rdi : char	*			s
@@ -23,20 +20,27 @@ SECTION .text
 	global		_ft_puts
 
 _ft_puts:
-	cmp			rdi, 0				; Check s
-	je			return_false
-
 	push		rdi
+
+	cmp			rdi, 0				; Check s
+	je			print_null
 
 	call		_ft_putstr			; Print str in %rdi
 
 	mov			rdi, 10
 	call		_ft_putchr			; Print chr in %rdi
 
+	jmp			return_true
+
+print_null:
+	;mov byte	[rel nullstr], 'x'
+	lea		rdi, [rel nullstr]		; Print "(null)\n"
+	call		_ft_putstr
+
+return_true:
 	pop			rdi					; Reset used registers
 	mov			rax, 1				; Return TRUE
 	ret
 
-return_false:
-	mov			rax, 0				; Return FALSE
-	ret
+SECTION .data
+	nullstr		db "(null)", 10
